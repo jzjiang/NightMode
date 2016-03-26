fdd46096008ea4313916d8c79ba01030cda8e3d5
Ivy Zhou <ivyzhou2012@hotmail.com>
2016-03-26 17:11:27 -0400

    change content script
    let's be reasonable here

--- content-script.js
+++ content-script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var myCanvas;
 var canvasContainer;
 
@@ -143,46 +142,3 @@ function clearArc(context, x, y, radius) {
 // 				}
 // 		}
  }
-||||||| merged common ancestors
-=======
-_ini();
-
-function _ini(){
-	window.onload = function(){	
-		
-		
-		$(document).ready(function(){
-			randomlyMoveImage();
-		});
-	
-
-		function randomlyMoveImage(){
-			var width = $(document).width()-500;
-			var height = $(document).height()-200;
-			var x = Math.floor((Math.random() * width) + 1);
-			var y = Math.floor((Math.random() * height) + 1);
-			
-			
-			$("#spooky>img").animate({
-				
-				left: x,
-				top: y
-			}, 0, function() {
-				console.log(x, y);
-			// Animation complete.
-					});
-
-		setTimeout(randomlyMoveImage, 3000);
-		}
-
-		
-		var div = document.createElement("div");
-		div.id = "spooky";
-			
-		var image = document.createElement("img");
-		image.src = chrome.extension.getURL('dancing-skeleton.gif');	
-		div.appendChild(image);
-		document.body.appendChild(div);	
-	}
-}
->>>>>>> origin/master
